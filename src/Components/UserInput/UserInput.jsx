37ec442581a518680f421585d958230c951096d6
Azadeh Galidari <agalidari@gmail.com>
2023-09-05 19:28:34 +0200

    added submitHandler, resetHandler and managed state for each input

--- src/Components/UserInput/UserInput.jsx
+++ src/Components/UserInput/UserInput.jsx
@@ -1,30 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UserInput = () => {
+    const [currentSavings, setCurrentSavings] = useState('');
+    const [yearlySavings, setYearlySavings] = useState('');
+    const [expectedInterest, setExpectedInterest] = useState('');
+    const [investmentDuration, setInvestmentDuration] = useState('');
+
+    const currentSavingsInputHandler = (e) => {
+        setCurrentSavings(e.target.value);
+    };
+
+    const yearlySavingsInputHandler = (e) => {
+        setYearlySavings(e.target.value);
+    };
+
+    const expectedInterestInputHandler = (e) => {
+        setExpectedInterest(e.target.value);
+    };
+
+    const investmentDurationInputHandler = (e) => {
+        setInvestmentDuration(e.target.value);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const inputData = {
+            inputCurrentSavings: currentSavings,
+            inputYearlySavings: yearlySavings,
+            inpuEexpectedInterest: expectedInterest,
+            inputInvestmentDuration: investmentDuration,
+        };
+
+        console.log(inputData);
+    };
+
+    const resetHandler = () => {
+        setCurrentSavings('');
+        setYearlySavings('');
+        setExpectedInterest('');
+        setInvestmentDuration('');
+    };
+
     return (
-        <form className='form'>
+        <form className='form' onSubmit={submitHandler}>
             <div className='input-group'>
                 <p>
                     <label htmlFor='current-savings'>Current Savings ($)</label>
-                    <input type='number' id='current-savings' />
+                    <input
+                        type='number'
+                        id='current-savings'
+                        value={currentSavings}
+                        onChange={currentSavingsInputHandler}
+                    />
                 </p>
                 <p>
                     <label htmlFor='yearly-contribution'>Yearly Savings ($)</label>
-                    <input type='number' id='yearly-contribution' />
+                    <input
+                        type='number'
+                        id='yearly-contribution'
+                        value={yearlySavings}
+                        onChange={yearlySavingsInputHandler}
+                    />
                 </p>
             </div>
             <div className='input-group'>
                 <p>
                     <label htmlFor='expected-return'>Expected Interest (%, per year)</label>
-                    <input type='number' id='expected-return' />
+                    <input
+                        type='number'
+                        id='expected-return'
+                        value={expectedInterest}
+                        onChange={expectedInterestInputHandler}
+                    />
                 </p>
                 <p>
                     <label htmlFor='duration'>Investment Duration (years)</label>
-                    <input type='number' id='duration' />
+                    <input
+                        type='number'
+                        id='duration'
+                        value={investmentDuration}
+                        onChange={investmentDurationInputHandler}
+                    />
                 </p>
             </div>
             <p className='actions'>
-                <button type='reset' className='buttonAlt'>
+                <button type='reset' className='buttonAlt' onClick={resetHandler}>
                     Reset
                 </button>
                 <button type='submit' className='button'>
